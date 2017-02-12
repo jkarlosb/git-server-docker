371e509e62889609c55769a3cb6e8ff95ce11e57
Akuario <jkarlos.b@gmail.com>
2017-02-12 18:06:39 +0100

    Added permission check in repos folder

--- start.sh
+++ start.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-cd /home/git
-
 # If there is some public key in keys folder
 # then it copies its contain in authorized_keys file
 if [ "$(ls -A /git-server/keys/)" ]; then
+  cd /home/git
   cat /git-server/keys/*.pub > .ssh/authorized_keys
   chown -R git:git .ssh
   chmod 700 .ssh
   chmod -R 600 .ssh/*
+fi
+
+# Checking permissions and fixing SGID bit in repos folder
+# More info: https://github.com/jkarlosb/git-server-docker/issues/1
+if [ "$(ls -A /git-server/repos/)" ]; then
+  cd /git-server/repos
+  chown -R git:git .
+  chmod -R ug+rwX .
+  find . -type d -exec chmod g+s '{}' +
 fi
 
 # -D flag avoids executing sshd as a daemon
