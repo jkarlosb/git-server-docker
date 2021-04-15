102d03d7425cff5c6d420da06adfdbffbf4892fd
Giacomo Tartari <giacomo.tartari@gmail.com>
2021-04-15 12:29:41 +0200

    start.sh: copy keys and repos to the tmpfs
    At startup we copy the repos and the keys to the tmpfs volumes.

--- start.sh
+++ start.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
-# If there is some public key in keys folder
-# then it copies its contain in authorized_keys file
-if [ "$(ls -A /git-server/keys/)" ]; then
-  cd /home/git
-  cat /git-server/keys/*.pub > .ssh/authorized_keys
-  chown -R git:git .ssh
-  chmod 700 .ssh
-  chmod -R 600 .ssh/*
-fi
+# copy kyes and repos to /git-server/keys and /git-server/repos
+cp -R /keys/* /git-server/keys/
+cp -R /http /git-server/repos/
+
+# copy its pub keys in authorized_keys file
+cd /home/git
+cat /git-server/keys/*.pub > .ssh/authorized_keys
+chown -R git:git .ssh
+chmod 700 .ssh
+chmod -R 600 .ssh/*
 
 # Checking permissions and fixing SGID bit in repos folder
 # More info: https://github.com/jkarlosb/git-server-docker/issues/1
-if [ "$(ls -A /git-server/repos/)" ]; then
-  cd /git-server/repos
-  chown -R git:git .
-  chmod -R ug+rwX .
-  find . -type d -exec chmod g+s '{}' +
-fi
+cd /git-server/repos
+chown -R git:git .
+chmod -R ug+rwX .
+find . -type d -exec chmod g+s '{}' +
 
 # -D flag avoids executing sshd as a daemon
 /usr/sbin/sshd -D
